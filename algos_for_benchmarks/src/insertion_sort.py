93ccf9a9403fbb40f5ae1e1e7ba16167e4cadb18
Dea María Léon <deamarialeon@gmail.com>
2023-09-12 17:35:06 +0200

    building again without timer

--- algos_for_benchmarks/src/insertion_sort.py
+++ algos_for_benchmarks/src/insertion_sort.py
@@ -1,9 +1,7 @@
 #Insertion Sort
 import time
 def sort(array: list[int]) -> list[int]:
-    # time.sleep(3)
     for i in range(1, len(array)):
-        
         key_item = array[i]
         j = i - 1
         while j >= 0 and array[j] > key_item:
